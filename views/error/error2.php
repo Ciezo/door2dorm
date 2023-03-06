d6cfd06f5ccf995ad1c11cfd81a7cd09721d3a47
Cloyd Van <abcd123.letters@gmail.com>
2023-03-06 21:18:58 +0800

    Stage error2 page

---
+++ views/error/error2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Error</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5 mb-3">Invalid API Request</h2>
+                    <div class="alert alert-danger">Sorry, the administrator has made an invalid request. Please <a href="../admin/admin-home.php" class="alert-link">go back to home</a> and try again.</div>
+                    <?php 
+                        date_default_timezone_set('Asia/Manila');
+                        $request_date = date("d/m/Y");
+                    ?>
+                    <span> Most recent request: <?php echo $request_date; ?>
+                    </span>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
