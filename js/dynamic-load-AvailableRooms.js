695e8422759a58485220e4a3670393ff2283c752
Cloyd Van <abcd123.letters@gmail.com>
2023-03-07 17:12:57 +0800

    Update to loading animation
    The loading animation only plays once

--- js/dynamic-load-AvailableRooms.js
+++ js/dynamic-load-AvailableRooms.js
@@ -1,9 +1,10 @@
 // Load up the dynamic component of available rooms every 5 seconds
 $(document).ready(function(){
-    setInterval(function(){
-        $("#loadingDiv").show(); // Display the loading GIF animation before content is fetched
-        $("#periodic-refresh-5secs").load("../components/dynamic/available-rooms.php", function() {
-            $("#loadingDiv").hide(); // Hide the loading animation after content is retrieved
-        });
-    }, 5000); // Refresh every 5 seconds
+    // Load up the dynamic content
+  $("#periodic-refresh-5secs").load("../components/dynamic/available-rooms.php", function() {
+    $("#loadingDiv").hide(); // Hide loading image after first load
+  });
+  setInterval(function(){
+    $("#periodic-refresh-5secs").load("../components/dynamic/available-rooms.php");
+  }, 5000); // Refresh the dynamic content every 5 seconds
 });
