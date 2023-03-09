3407cfaccc2c93e805ff3d54aea01abda83dd71c
Cloyd Van <abcd123.letters@gmail.com>
2023-03-10 01:52:49 +0800

    Prepared view template for tenant webpapges

---
+++ views/tenant/tenant-payment.php
@@ -0,0 +1,76 @@
+<?php
+require("../../config.php");
+session_start(); 
+// Check if tenant is logged in.
+if (!isset($_SESSION["tenant-username"])) {
+    // If not logged in, then redirect to log-in page.
+    header("location: tenant-login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- Bootstrap from https://getbootstrap.com/ -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c36559a51c.js" crossorigin="anonymous"></script>
+
+    <!-- CSS Global theming and styles -->
+    <link href="../../css/globals.css" rel="stylesheet">
+
+    <!-- jQuery-->
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <style>
+        .wrapper {
+            padding-top: 50px;
+            margin: 0 auto;
+            width: 1050px;
+        }
+        .navbar .navbar-brand {
+            padding-left: 35px;
+        }
+        .navbar .collapse .navbar-nav .active {
+            background-color: white;
+            border-radius: 5px;
+            color:black;
+            box-shadow: 0 5px 10px rgb(0 0 0 / 0.2);
+        }
+    </style>
+</head>
+<body>
+    <!-- Bootstrap navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">      
+            <i class="fa-solid fa-house-user"></i>
+                Welcome, <?php echo $_SESSION["tenant-username"]?>! 
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="tenant-home.php">Home <span class="sr-only"></span></a>
+                <a class="nav-item nav-link" href="tenant-account.php">My Account </span></a>
+                <a class="nav-item nav-link active" href="#">Payment </span></a>
+                <a class="nav-item nav-link" href="tenant-securityLogs.php">Security Logs </span></a>
+                <a class="nav-item nav-link" href="tenant-messages.php">Message </span></a>
+                <a class="nav-item nav-link" href="../../components/custom/logout.php">Logout </span></a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Content goes here -->
+    <div class="wrapper">
+       hello
+    </div>
+</body>
+</html>
