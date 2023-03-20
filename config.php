1bcc08df59ffe6c185ea3dfdc8a620d83f4f8405
Cloyd Van <abcd123.letters@gmail.com>
2023-03-21 07:33:04 +0800

    Set up env variables

--- config.php
+++ config.php
@@ -4,17 +4,30 @@
  -->
 
 <?php
+require 'vendor/autoload.php';
+
+try {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+} catch (Dotenv\Exception\InvalidPathException $e) {
+    // handle exception if the .env file is not found or readable
+    echo 'Could not find or read the .env file: ' . $e->getMessage();
+} catch (Dotenv\Exception\ValidationException $e) {
+    // handle exception if validation fails for any of the environment variables
+    echo 'Validation failed for one or more environment variables: ' . $e->getMessage();
+}
+
+$host = $_ENV["DB_HOST"];
+$user = $_ENV["DB_USER"];
+$password = $_ENV["DB_PASS"];
 // Auto-generated credentials by ClearDB MySQL database at Heroku
-$host = "us-cdbr-east-06.cleardb.net"; 
-$user = "b08c35ce866050";
-$password = "d2655c71";
-$database = "heroku_9ee0e413f16e154";
+$database = $_ENV["DB_SCHEMA"];
 
 // database link connection
 $conn = new mysqli($host, $user, $password, $database);
 
 
-/** Establish connection with error checking */
+// /** Establish connection with error checking */
 if ($conn->connect_error) {
     echo '
     <script>
