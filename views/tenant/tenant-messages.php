e24eb5d831dabba0f2cc0f1249c73ea4553c55c4
Cloyd Van <abcd123.letters@gmail.com>
2023-06-12 18:16:04 +0800

    Update Tenant Message Admin Page

--- views/tenant/tenant-messages.php
+++ views/tenant/tenant-messages.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION["tenant-username"])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html data-theme="light" lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,6 +21,8 @@ if (!isset($_SESSION["tenant-username"])) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/c36559a51c.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.51.6/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- CSS Global theming and styles -->
     <link href="../../css/globals.css" rel="stylesheet">
@@ -67,29 +69,41 @@ if (!isset($_SESSION["tenant-username"])) {
 
     <!-- Content goes here -->
     <div class="container">
-        <div class="tenant-send-msg">
-            <h2>Create your message: Send feedback, file complaints or reports, or request for repairs</h2>
-            <p>Issue your inquiries to the admin!</p>
-            <div class="card px-5 w-100">
-                <form action="../../api/messages/send.php" method="POST">
-                    <div class="form-group">
-                        <label for="Subject of message">Select a message subject</label>
-                        <select name="msg-subj" class="form-control">
-                            <option value="General">General</option>
-                            <option value="Repairs">Repairs</option>
-                            <option value="Feedback">Feedback</option>
-                            <option value="Report">Report</option>
-                        </select>
-                    </div>
-                    <br>
-                    <div class="form-group">
-                        <label for="Message body">My Message</label>
-                        <textarea name="msg-body" placeholder="Write your message here" class="form-control" rows="10" required></textarea>
-                    </div>
-                    <br>
-                    <!-- Send message to database -->
-                    <input type="submit" name="send-msg" class="btn btn-outline-primary" value="Send">
-                </form>
+        <div class="hero min-h-screen mb-4" style="background-image: url(https://site.surveysparrow.com/wp-content/uploads/2019/12/10-Sure-shot-ways-to-Deliver-Good-Customer-Service.png);">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content flex flex-col lg:flex-row lg:space-x-8">
+            <div class="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <form action="../../api/messages/send.php" method="POST">
+                        <div class="form-control mb-4">
+                            <label class="label">
+                                <span class="label-text">Select a message subject</span>
+                            </label>
+                            <select name="msg-subj" class="select select-bordered w-full max-w-xl">
+                                <option>General</option>
+                                <option>Repairs</option>
+                                <option>Feedback</option>
+                                <option>Reports</option>
+                            </select>
+                        </div>
+                        
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">My Message</span>
+                            </label>
+                            <textarea name="msg-body" placeholder="Write your message here" class="textarea textarea-warning form-control" rows="10" required></textarea>
+                        </div>
+                
+                        <div class="form-control mt-6">
+                            <input type="submit" name="send-msg" class="btn btn-warning" value="Send" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+            <div class="text-center lg:text-left">
+                <h1 class="text-5xl font-bold mb-6 text-white">Issue your inquiries to the admin!</h1>
+                <p class="text-white"> Create your message: Send feedback, file complaints or reports, or request for repairs</p>
             </div>
         </div>
     </div>
