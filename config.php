0cac99ff086c8ae933643578c301d6bd75e884ce
Cloyd Van <abcd123.letters@gmail.com>
2024-01-17 17:13:41 +0800

    Remove useless console logs on config

--- config.php
+++ config.php
@@ -9,21 +9,12 @@ require 'vendor/autoload.php';
 try {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
-    echo '<script>';
-    echo    'console.log("ENVIRONMENT VARIABLES LOADED!")';
-    echo '</script>';
 } catch (Dotenv\Exception\InvalidPathException $e) {
     // handle exception if the .env file is not found or readable
     // echo 'Could not find or read the .env file: ' . $e->getMessage();
-    echo '<script>';
-    echo    'console.log("Could not find or read the .env file: ")'.$e->getMessage();
-    echo '</script>';
 } catch (Dotenv\Exception\ValidationException $e) {
     // handle exception if validation fails for any of the environment variables
     // echo 'Validation failed for one or more environment variables: ' . $e->getMessage();
-    echo '<script>';
-    echo    'console.log("Validation failed for one or more environment variables: ")'.$e->getMessage();
-    echo '</script>';
 }
 
 $host = $_ENV["DB_HOST"];
@@ -38,18 +29,11 @@ $conn = new mysqli($host, $user, $password, $database);
 
 // /** Establish connection with error checking */
 if ($conn->connect_error) {
-    echo '
-    <script>
-        console.log("Could not establish connection to Heroku or EXCEEDED MAXIMUM CONNECTIONS!");
-    </script>'; 
     die("Error connecting".$conn->connect_error);
 }
 
 else {
-    echo '
-    <script>
-        console.log("Connection SUCCESSFUL!");
-    </script>'; 
+
 }
 
 $host_bk = $_ENV["DB_HOST_BK"];
@@ -60,17 +44,10 @@ $schema_bk = $_ENV["DB_SCHEMA_BK"];
 // Init connection_bk 
 $conn_bk = new mysqli($host_bk, $user_bk, $pass_bk, $schema_bk);
     if ($conn_bk->connect_error) {
-        echo '
-        <script>
-            console.log("Could not establish connection to Heroku or EXCEEDED MAXIMUM CONNECTIONS!");
-        </script>'; 
         die("Error connecting".$conn_bk->connect_error);
     }
 
     else {
-        echo '
-        <script>
-            console.log("Connection to backup server SUCCESSFUL!");
-        </script>'; 
+
     }
 ?>
