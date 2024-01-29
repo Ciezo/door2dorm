bf23b14c6d9a20499b1cbfa3f19a9d306a1327b6
Cloyd Van <abcd123.letters@gmail.com>
2024-01-29 13:47:03 +0800

    Update how an administrator is registered

--- sql/register_admin.sh
+++ sql/register_admin.sh
@@ -4,8 +4,8 @@
 #       Hardcode the admin credentials into Admin table
 
 host="us-cluster-east-01.k8s.cleardb.net" 
-user="b6242f07ae4393"
-password="c5ba3043"
-database="heroku_f47e36bc84bb2cb"
+user="bd606795766db9"
+password="3b1ba14e"
+database="heroku_098a2fcd8fadcea"
 
 cat "admin_credentials.sql" | mysql -h "$host" -u "$user" "-p$password" "$database"
