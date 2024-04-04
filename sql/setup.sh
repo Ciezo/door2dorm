fd41beb9a47c6920d2ce1d38e3b0e5fe76e644e9
Cloyd Van <abcd123.letters@gmail.com>
2024-04-04 15:48:45 +0800

    Remove Heroku integration
    Heroku is really expensive right now...
    So, I'm switching to localhost

--- sql/setup.sh
+++ sql/setup.sh
@@ -1,16 +1,15 @@
 #!/bin/sh 
 # Filename: setup.sh
 # Description: 
-#       A script that can automate the creation of a database schema 
+#       A script that can automate the creation of a PRIMARY DATABASE schema 
 #       with initialized default values on the localhost. 
 #       This shall run the .sql script
 
 # Credentials
-# App name: door2dorm-backend
-host="us-cluster-east-01.k8s.cleardb.net" 
-user="bd606795766db9"
-password="3b1ba14e"
-database="heroku_098a2fcd8fadcea"
+host="localhost" 
+user="root"
+password="cloyd27feb2002"
+database="door2dorm"
 
-echo "Creating schema to the remote database!"
+echo "Creating schema to the localhost database!"
 cat "schema.sql" | mysql -h "$host" -u "$user" "-p$password" "$database"
